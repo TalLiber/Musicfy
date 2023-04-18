be88580b2dcef4c4f869c9d635c0e30ad0d77c89
Eran <peled2k@gmail.com>
2023-04-18 18:57:08 +0300

    added next and prev buttons to header

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useNavigate, useLocation, NavLink } from 'react-router-dom'
 
-export const Header = () => {
- return (
-  <div className='header-container'>Header</div>
- )
+import SvgIcon from './SvgIcon'
+
+export const Header = (props) => {
+
+    const navigator = useNavigate()
+    const location = useLocation()
+    const isPrev = useRef(location.key === 'default')
+    const isNext = useRef(location.key === 'default')
+
+    useEffect(() =>{
+        isPrev.current = location.key !== 'default'
+        isNext.current = location.key === 'default'
+    },[location.key])
+    
+    const handleClick = (num) => {
+        // if(num === 1) navigator('/search') 
+        // else navigator(num)
+        navigator(num)
+    }
+    return (
+        <section className='header-container flex'>
+            <div className='action flex'>
+                <button className='btn-action' disabled={isPrev.current} onClick={() => handleClick(-1)}>
+                    {SvgIcon({iconName:'prev'})}
+                </button>
+                <button className='btn-action' disabled={isNext.current} onClick={() => handleClick(1)}>
+                    {SvgIcon({iconName:'next'})}
+                </button>
+            </div>
+            <div className='login-container'>
+                <button className='btn-signup'>Sign up</button>
+                <button className='btn-login'>Log in</button>
+            </div>
+            {/* <NavLink to="/search">Search</NavLink>   */}
+        </section>
+    )
 }
