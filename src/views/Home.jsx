a0b399c2e4ddc0048e3047d0a6d30f1844488f44
Eran <peled2k@gmail.com>
2023-04-25 18:09:26 +0300

    pixel perfect home cmp

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { HomeList } from '../cmps/HomeList'
@@ -6,15 +6,36 @@ import { HomeList } from '../cmps/HomeList'
 export const Home = () => {
 
     const playlists = useSelector(state => state.playlistModule.playlists)
+    const categories = [{id:1, name:'Focus',playlists},{id:2, name:'Pop', playlists}]
+    const containerRef = useRef(null)
+
+    const [isVisible,setIsVisible] = useState(false) 
+    const options ={
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0
+    }
+    const cb = () => {
+        setIsVisible(prevState => !prevState)
+    }
 
     useEffect(() => {
-        // console.log('playlists',playlists)
-    }, [])
+        const observer = new IntersectionObserver(cb,options)
+        if(containerRef.current) observer.observe(containerRef.current)
+
+        return () => {
+            if(containerRef.current) observer.unobserve(containerRef.current)
+        }
+    }, [containerRef])
+    // }, [])
 
 
     return (
         <div className='home-container'>
-            <HomeList playlists={playlists}/>
+            <div ref={containerRef}>{isVisible? 'yes' : 'no'}</div>
+            {categories.map((category,idx) => {
+                return <HomeList category={category} key={idx}/>
+            })}
         </div>
     )
 }
