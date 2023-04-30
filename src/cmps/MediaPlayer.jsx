e85ee8c7330d7f10b3bde95f7d61d483f795fbef
Tal Liber <tal.liber@gmail.com>
2023-04-30 18:41:22 +0300

    player ui

--- src/cmps/MediaPlayer.jsx
+++ src/cmps/MediaPlayer.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+
+import SvgIcon from './SvgIcon'
 
 export const MediaPlayer = () => {
- return (
-  <div className='player-container'>MediaPlayer</div>
- )
+
+  const currSong = useSelector(state => state.playlistModule.currSong)
+  useEffect(() => {
+    console.log(currSong, 'currSong');
+  }, currSong)
+  return (
+    <div className='player-container'>
+      <div className="song-container">
+        <img src={currSong.imgUrl} />
+        <div className="song-details">
+          <p className="song-title">{currSong.title}</p>
+          <p className="song-artist">{currSong.artist}</p>
+        </div>
+        <i>{SvgIcon({ iconName: 'heart-empty' })}</i>
+      </div>
+      <div className="player-control">
+        <div className="control-btns">
+          <div className="side-btns left-side">
+            <i>{SvgIcon({ iconName: 'shuffle' })}</i>
+            <i>{SvgIcon({ iconName: 'prev-song' })}</i>
+          </div>
+          <i className="play-btn">
+            {SvgIcon({ iconName: 'player-play' })}
+          </i>
+          <div className="side-btns right-side">
+            <i>{SvgIcon({ iconName: 'next-song' })}</i>
+            <i>{SvgIcon({ iconName: 'repeat' })}</i>
+          </div>
+        </div>
+        <div className="playback-bar">
+          <div className="progress-time elapsed">-:--</div>
+          <div className="progress-bar"></div>
+          <div className="progress-time duration">-:--</div>
+        </div>
+      </div>
+      <div className="side-container">
+        <i>{SvgIcon({ iconName: 'lyrics' })}</i>
+        <i>{SvgIcon({ iconName: 'volume-high' })}</i>
+      </div>
+    </div>
+  )
 }
--- src/store/reducers/playlists.reducer.js
+++ src/store/reducers/playlists.reducer.js
@@ -1,10 +1,14 @@
 const INITIAL_STATE = {
     playlists: null,
-    filterBy: {
-
-    }
+    currSong: {
+        addedAt: '2023-04-11T18:10:48Z',
+        title: 'Call Me Maybe',
+        artist: 'Carly Rae Jepsen',
+        album: 'Kiss (Deluxe)',
+        imgUrl: 'https://i.scdn.co/image/ab67616d0000b273a111f7769013f1731e9c697c',
+    },
+    filterBy: {},
 }
-
 
 export function playlistReducer(state = INITIAL_STATE, action) {
     // debugger
@@ -12,31 +16,34 @@ export function playlistReducer(state = INITIAL_STATE, action) {
         case 'SET_PLAYLISTS':
             return {
                 ...state,
-                playlists: [...action.playlists]
+                playlists: [...action.playlists],
             }
         case 'ADD_PLAYLIST':
             return {
                 ...state,
-                playlists: [action.playlist, ...state.playlists]
+                playlists: [action.playlist, ...state.playlists],
             }
         case 'REMOVE_PLAYLIST':
             return {
                 ...state,
-                playlists: state.playlists.filter(playlist => playlist._id !== action.playlistId)
+                playlists: state.playlists.filter(
+                    (playlist) => playlist._id !== action.playlistId
+                ),
             }
         case 'UPDATE_PLAYLIST':
             return {
                 ...state,
-                playlists: state.playlists.map(playlist => playlist._id === action.playlist._id ? action.playlist : playlist)
+                playlists: state.playlists.map((playlist) =>
+                    playlist._id === action.playlist._id ? action.playlist : playlist
+                ),
             }
         case 'SET_FILTER_BY':
             return {
                 ...state,
-                filterBy: {...action.filterBy }
+                filterBy: {...action.filterBy },
             }
 
         default:
             return state
     }
-
 }
