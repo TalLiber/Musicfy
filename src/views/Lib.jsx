1d503de44b9041c7d1d832618cf6930781e04a4c
Tal Liber <tal.liber@gmail.com>
2023-05-04 17:26:41 +0300

    scroll changes

--- src/views/Lib.jsx
+++ src/views/Lib.jsx
@@ -1,5 +1,9 @@
 export const Lib = () => {
   return (
-    <div>PlaylistLibrary</div>
+    <div>PlaylistLibrary
+      <iframe id="player" type="text/html" width="640" height="390"
+        src="https://www.youtube.com/watch?v=-2U0Ivkn2Ds&list=PLLv_121dX6AWloBvVcsuHwvaTo5XDtRVa&index=4"
+        frameborder="0"></iframe>
+    </div>
   )
 }
